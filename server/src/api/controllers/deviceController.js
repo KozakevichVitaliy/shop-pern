586b4a41fe049235afe87fec32a3da455af3e482
Vitaliy Kozakevich <kozakevich.vitaliy@gmail.com>
2021-09-10 09:20:44 +0300

    finished device controler

--- server/src/api/controllers/deviceController.js
+++ server/src/api/controllers/deviceController.js
@@ -1,6 +1,6 @@
 const uuid = require('uuid')
 const path = require('path')
-const { Device } = require('../../db/models')
+const { Device, DeviceInfo } = require('../../db/models')
 const ApiError = require('../error/ApiError')
 class DeviceController {
   async create(req, res, next) {
@@ -9,8 +9,17 @@ class DeviceController {
       const { img } = req.files
       let filename = uuid.v4() + '.jpg'
       img.mv(path.resolve(__dirname, '..', '..', 'static', filename))
-
       const device = await Device.create({ name, price, brandId, typeId, img: filename })
+
+      if (info) {
+        info = JSON.parse(info)
+        info.forEach(i => 
+          DeviceInfo.create({
+            title: i.title,
+            description: i.description,
+            deviceId: device.id
+          }))
+      }
 
       return res.json(device)
     } catch (error) {
@@ -42,7 +51,20 @@ class DeviceController {
     }
   }
   async getOne(req, res) {
-    
+    const { id } = req.params
+    try {
+      const device = await Device.findOne(
+        {
+          where: {id} ,
+          include: [{ model: DeviceInfo, as: 'info' }]
+        }
+      )
+      
+      return res.json(device)
+    } catch (error) {
+      next(ApiError.internal(error.message))
+    }
+
   }
 }
 
--- server/src/api/controllers/userController.js
+++ server/src/api/controllers/userController.js
@@ -1,6 +1,22 @@
+const bcrypt = require('bcrypt')
+const ApiError = require("../error/ApiError")
+const { User, Basket } = require('../../db/models')
+
 class UserController {
-  async signup() {
-    
+  async signup(req, res, next) {
+    const { email, password } = req.body
+    if (!email && !password) {
+      return next(ApiError.badRequest('Incorrect credential data!'))
+    }
+
+    const candidate = await User.findOne({  where: {email}})
+    if (candidate) {
+      return next(ApiError.badRequest('Use already exists!'))
+    }
+
+    const hashPassword = bcrypt.hash(password, 5)
+    const user = await User.create({ email, role, password })
+    const basket = await Basket.create({ userId: user.id })
   }
   async signin() {
     
